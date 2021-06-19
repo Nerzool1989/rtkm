9e923750818ee17390af2e011b19249d18bb2158
Nerzool1989 <nerzool1989@yandex.ru>
2021-06-19 19:38:04 +0300

    reve

---
+++ src/reduxtk/store.js
@@ -0,0 +1,7 @@
+import {configureStore, getDefaultMiddleware} from '@reduxjs/toolkit';
+import topReducer from '../reduxtk/topReducer';
+
+const reducer = {
+    topReducer
+}
+
---
+++ src/reduxtk/topReducer.js
@@ -0,0 +1,4 @@
+
+const topReducer = (state = {}, action) => {return state};
+
+export default topReducer;
